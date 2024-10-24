dd027eaf42e5f9a5249b4a3153c3ff4b1e2e4f38
zini23 <pedro.correia.piruzzo@gmail.com>
2024-10-24 17:36:12 +0100

    Added input validation for answer selection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gameRules import rules
-import questions
+from questions import *
 
 def main():
     print("****WELCOME TO BRAIN BLITZ****")
@@ -11,8 +11,8 @@ def main():
             game()  # Automatically start the game after displaying the rules
             break
         elif user_input == "n":
-            game()  # Skip the rules and start the game
             ready()
+            game()  # Skip the rules and start the game
             break
         else:
             print("Invalid input. Please enter 'y' for yes or 'n' for no.")
@@ -26,15 +26,40 @@ def ready():
 def game():
 
     n_question = 0
-    print("Starting the game...")
+    questions = load_questions()
+    difficulty = ["easy", "medium", "hard", "super hard"]
+    print("Starting the game...\n")
 
     while n_question < 12:
-         
+        question_num(n_question)
+        # Choose difficulty based on question number
+        if n_question < 3:
+            question = get_random_question(questions, difficulty[0]) 
+        elif n_question >= 3 and n_question < 6:
+            question = get_random_question(questions, difficulty[1])  
+        elif n_question >= 6 and n_question < 9:
+            question = get_random_question(questions, difficulty[2])  
+        else:
+            question = get_random_question(questions, difficulty[3])
         if question:
-                    print(question['question'])
-                    print("Choices:")
-                    for idx, choice in enumerate(question['choices'], 1):
-                            print(f"{idx}. {choice}")
+            print(question['question'])
+            print("Choices:")
+            for idx, choice in enumerate(question['choices'], 1):
+                print(f"{idx}. {choice}")
 
+            while True:
+                         
+                answer = input("Lock in your answer: ").upper()
+                if answer not in ['A', 'B', 'C', 'D']:
+                    print("Invalid input! Please enter A, B, C, or D.")
+                else:
+                    break
+
+            if answer != question['correct_answer']:
+                print(f"That's incorrect!\nThe correct answer was {question['correct_answer']}. Better luck next time!\n")
+            else:
+                print(f"Congratulations! You got it right! The correct answer is {question['correct_answer']}.\n")
+            n_question += 1            
+        
 
 main()
--- questions.py
+++ questions.py
@@ -51,7 +51,7 @@ def get_random_question(questions, difficulty):
     return None
 
 def question_num(n_question):
-    positions = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "Last"]
+    positions = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "Last"]
 
     if n_question < len(positions):
         print(positions[n_question] + " question")

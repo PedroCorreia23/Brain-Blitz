a4036a8a5df40deb0e899acaec6cb0e2c55d7c2b
zini23 <pedro.correia.piruzzo@gmail.com>
2024-10-26 15:14:40 +0100

    Added level logic and improved hint functionality

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def timer(stop_event):
 def game():
 
     hints = 7
+    level = 0
+    money_levels = ["0€", "200€", "500€", "1000€", "3000€", "10000€", "50000€" ]
     n_question = 0
     questions = load_questions()
     difficulty = ["easy", "medium", "hard", "super hard"]
@@ -50,11 +52,11 @@ def game():
     while n_question < 12:
         question_num(n_question)
         # Choose difficulty based on question number
-        if n_question < 3:
+        if level <= 2:
             question = get_random_question(questions, difficulty[0]) 
-        elif n_question >= 3 and n_question < 6:
+        elif level == 3 or level == 4:
             question = get_random_question(questions, difficulty[1])  
-        elif n_question >= 6 and n_question < 9:
+        elif level ==5 or level == 6:
             question = get_random_question(questions, difficulty[2])  
         else:
             question = get_random_question(questions, difficulty[3])
@@ -90,13 +92,29 @@ def game():
             timer_thread.join()
 
             if answer != question['correct_answer']:
-                print(f"That's incorrect!\nThe correct answer was {question['correct_answer']}. Better luck next time!\n")
-                hints -= 3
-                print(f"Numero: {hints}")
                 
+                if hints >= 3:
+                    hints -= 3
+                elif hints == 2:
+                    hints -=2
+                    level -= 1
+                elif hints == 1:
+                    hints -=1
+                    level -= 2
+                elif hints == 0:
+                    level -= 3 
+                
+                print(f"That's incorrect!\nThe correct answer was {question['correct_answer']}. Better luck next time!\n")
+                print(f"Numero de Hints: {hints}\nMoney Level: {money_levels[level]}")
+
             else:
+                if level == 6:
+                    pass
+                else:
+                    level += 1
+
                 print(f"Congratulations! You got it right! The correct answer is {question['correct_answer']}.\n")
-                print(f"Numero: {hints}")
+                print(f"Numero de Hints: {hints}\nMoney Level: {money_levels[level]}")
 
             n_question += 1       
 
